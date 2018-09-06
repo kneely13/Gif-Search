4b3915eb1fadfd75bad97bf5b48c530ee23f2f83
Kristian Neely <32805776+kneely13@users.noreply.github.com>
2018-09-06 15:02:48 -0400

    trying to figure out image issue

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,30 @@
-var topics=["Cessna","F-14"]
+var topics=["Cessna","F-14"];
+
+
+$("#airplaneButtons").on('click', function(){
+
+    var aircraft = $(this).attr("data-name");
+    
+    var queryURL = "https://api.giphy.com/GET/v1/gifs/search?api_key=3CYKBkbY2Z6k94oEvOxFIYwZEMGDc2JV&q="+aircraft+"&limit=10&offset=0&rating=G&lang=en";
+
+    $.ajax({
+        url:queryURL,
+        method: "GET"
+    }).then(function(response){
+
+        var imgUrl='response.data.image_original_url';
+
+        var $aircraftImage = $("<img>");
+
+      // Setting the aircraftImage src attribute to imageUrl
+        $aircraftImage.attr("src", imgUrl);
+        $aircraftImage.attr("alt", "aircraft image");
+
+        //Prepend the aircraftImage to the airplanes div
+        $("#airplanes").prepend($aircraftImage);
+
+    })
+});
 
 function renderButtons() {
 
@@ -15,36 +41,9 @@ function renderButtons() {
             //Providing the buttons text with the value of the aircraft at index i
             $b.text(topics[i]);
             //This will add the button to my html under the div I named as an id of airplaneButtons
-            $("#airplaneButtons").append($b)
+            $("#airplaneButtons").append($b);
         }
 }
-
-$("#airplaneButtons").on('click', function(){
-
-    
-    var api_key= "3CYKBkbY2Z6k94oEvOxFIYwZEMGDc2JV";
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=3CYKBkbY2Z6k94oEvOxFIYwZEMGDc2JV&q=&limit=25&offset=0&rating=G&lang=en";
-
-    $.ajax({
-        url:queryURL,
-        method: "GET"
-    }).then(function(response){
-
-        var imgUrl=response.data.image_original_url;
-
-        var aircraftImage = $("<img>");
-
-      // Setting the aircraftImage src attribute to imageUrl
-        aircraftImage.attr("src", imgUrl);
-        aircraftImage.attr("alt", "aircraft image");
-
-        //Prepend the aircraftImage to the airplanes div
-        $("#airplanes").prepend(aircraftImage);
-
-    })
-});
-
-
 
 $("#addAircraft").on("click", function(event){
 
@@ -59,7 +58,9 @@ $("#addAircraft").on("click", function(event){
 
     // //call the render button now to process the movie array
     renderButtons();
-})
+});
 
+// $(document).on("click", ".aircraft", displayMovieInfo);
 
 renderButtons();
+
